cde6619d05cf64a1f3b909ea3217ffe5293d504b
SaadQamar112 <saadbinadeem@gmail.com>
2025-03-26 19:16:45 +0500

    activity 1

---
+++ mm..py
@@ -0,0 +1,16 @@
+#creating variables
+tree1=99
+tree2=89
+tree3=92
+tree4=42
+tree5=78
+
+#Adding the variables
+sum=tree1+tree2+tree3+tree4+tree5
+
+print ("",sum)
+
+#finding the averages
+
+average=sum/5
+print (average)
