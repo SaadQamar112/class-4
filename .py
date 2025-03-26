52d9b0604c767f798eb710ffbc11e95884750f64
SaadQamar112 <saadbinadeem@gmail.com>
2025-03-26 19:48:19 +0500

    act3

---
+++ .py
@@ -0,0 +1,14 @@
+#input from  the student
+print("enter the marks of your 3 subjests")
+
+computer=int(input("computer:"))
+science=int(input("science:"))
+math=int(input("math:"))
+
+sum=computer+science+math
+print(sum)
+
+#printing the percentage
+perc=(sum/300)*100
+print(perc)
+
